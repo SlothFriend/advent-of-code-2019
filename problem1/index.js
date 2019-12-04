e17a8b9d847146674f73d0b3aa74b383493ca600
David Clark <davidclark314@gmail.com>
2019-12-03 21:33:49 -0700

    Problem 1

---
+++ problem1/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+
+const result = fs.readFileSync('./input.txt', 'utf8')
+
+const masses = result.split('\n').map(n => parseInt(n)).filter(n => !isNaN(n))
+
+// const fuelNeeded = masses.reduce((total, mass) => {
+//   const toAdd = Math.floor(mass / 3) - 2
+//   return total + toAdd
+// }, 0)
+
+// const message = `Total fuel needed: ${fuelNeeded}`
+// console.log(message)
+
+// return message
+
+
+/** Part 2 **/
+
+const getRecursiveFuel = (mass) => {
+  const toAdd = Math.max(Math.floor(mass / 3) - 2, 0)
+  if (toAdd === 0) return 0
+  return toAdd + getRecursiveFuel(toAdd)
+}
+
+const fuelNeeded = masses.reduce((total, mass) => {
+  return total + getRecursiveFuel(mass)
+}, 0)
+
+const message = `Total fuel needed: ${fuelNeeded}`
+console.log(message)
+
+return message
